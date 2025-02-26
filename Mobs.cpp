82dcd98822e10fa16acf4c718d503fb90ec88d21
YuppiDuck <bedisgolovi695@gmail.com>
2025-02-26 20:20:28 +0500

    first_project

---
+++ Mobs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+#include <random>
+using namespace std;
+
+class creature {
+public:
+    string: name;
+    int: damage;
+    int: health;
+
+
+}
---
+++ main_character.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+#include <random>
+using namespace std;
+
+class main_char {
+public:
+    int: mc_dmg;
+    int: mc_atk;
+    int: base_hp;
+    
+};
+
